9b19dbb40dd8a045443d118f087fc9475c05e0b1
palucaville <108471545+palucaville@users.noreply.github.com>
2023-05-15 18:12:32 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import random
+
+def random_choice():
+  return random.choice(["rock", "paper", "scissors"])
+
+def winner(user_choice, computer_choice):
+  if user_choice == computer_choice:
+    return "Tie!"
+  elif user_choice == "rock" and computer_choice == "scissors":
+    return "You won!"
+  elif user_choice == "scissors" and computer_choice == "paper":
+    return "You won!"
+  elif user_choice == "paper" and computer_choice == "rock":
+    return "You won!"
+  else:
+    return "You lost!"
+    
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/play", methods=["POST"])
+def play():
+  user_choice = request.form["choice"]
+  computer_choice = random.choice(["rock", "paper", "scissors"])
+
+  if user_choice == computer_choice:
+    result = "Tie!"
+  elif user_choice == "rock" and computer_choice == "scissors":
+    result = "You won!"
+  elif user_choice == "scissors" and computer_choice == "paper":
+    result = "You won!"
+  elif user_choice == "paper" and computer_choice == "rock":
+    result = "You won!"
+  else:
+    result = "You lost!"
+
+  return render_template("result.html", user_choice=user_choice, computer_choice=computer_choice, result=result)
+
+if __name__ == "__main__":
+  app.run(debug=True)
