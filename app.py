0d6d19b69971e3ba2787b3f8deb86367cc59dc66
Luca Mastrodicasa <108471545+palucaville@users.noreply.github.com>
2023-11-02 14:49:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import random
 
+global last_result
+
 def random_choice():
   return random.choice(["rock", "paper", "scissors"])
 
@@ -15,12 +17,14 @@ def winner(user_choice, computer_choice):
     return "You won!"
   else:
     return "You lost!"
-    
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+
+  last_result = ""
+  return render_template("index.html", last_result=last_result)
 
 @app.route("/play", methods=["POST"])
 def play():
@@ -37,8 +41,17 @@ def play():
     result = "You won!"
   else:
     result = "You lost!"
-
+  global last_result
+  last_result = result
   return render_template("result.html", user_choice=user_choice, computer_choice=computer_choice, result=result)
+
+@app.errorhandler(400)
+def not_found_error(error):
+    return render_template('400.html'), 400
+
+@app.errorhandler(500)
+def not_found_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
   app.run(debug=True)
